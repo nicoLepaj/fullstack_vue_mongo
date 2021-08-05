d27b0d7f00adc2957317fdf809cd10467821eeff
Nicolas <nicolas.lepaj@gmail.com>
2021-08-05 09:52:19 +0200

    frontend added

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -5,45 +5,41 @@ const router = express.Router();
 
 // Get posts
 router.get('/', async (req, res) => {
-	const posts = await loadPostsCollection();
-	res.send(await posts.find({}).toArray());
+		const posts = await loadPostsCollection();
+		res.send(await posts.find({}).toArray());
 });
+
+async function loadPostsCollection() {
+		const client = await mongodb.MongoClient.connect(
+			'mongodb+srv://nicoLepaj:nicoLepajMongo@cluster0.sv88d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: true,
+			}
+		);
+		return client.db('myFirstDatabase').collection('posts');
+}
 
 // Add post
 router.post('/', async (req, res) => {
 	const posts = await loadPostsCollection();
-	await posts.insertOne({
+	const postToInsert = {
 		text: req.body.text,
 		createdAt: new Date(),
-	});
-	res.status(201).send();
+	};
+	await posts.insertOne(postToInsert);
+	res.status(201).send(postToInsert);
 });
 
 // Update post
-
 
 // Delete post
 router.delete('/:id', async (req, res) => {
 	const posts = await loadPostsCollection();
 	try {
-		// await posts.deleteOne({ _id: objectId(req.params.id) });
-		console.log(req.params.id);
 		await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
 	} catch (error) {
-		console.log(error);
 	}
 	res.status(200).send();
 });
-
-async function loadPostsCollection() {
-	const client = await mongodb.MongoClient.connect(
-		'mongodb+srv://nicoLepaj:nicoLepajMongo@cluster0.sv88d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-		{
-			useNewUrlParser: true,
-		}
-	);
-
-	return client.db('myFirstDatabase').collection('posts');
-}
 
 module.exports = router;
